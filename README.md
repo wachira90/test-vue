b837bbdda2e8c7a1bbc0ee93d25d2e2251edf851
Wachira Duangdee <wcrcorp@gmail.com>
2023-09-09 23:05:16 +0700

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,51 @@
-# test-vue
-test-vue
+# hello-world
+
+node v18.16.1
+
+yarn 1.22.19
+
+npm 9.5.1
+
+## Project setup
+
+```sh
+yarn install
+```
+
+### start hot-reloads for development
+
+```sh
+yarn run serve --mode production
+yarn run serve --mode development
+yarn run serve --mode uat
+yarn run serve --mode test
+```
+
+### build
+
+```sh
+yarn run build --mode production
+yarn run build --mode development
+yarn run build --mode development
+yarn run build --mode test
+```
+
+### Lints and fixes files
+
+```sh
+yarn lint
+```
+
+### Customize configuration
+
+See [Configuration Reference](https://cli.vuejs.org/config/).
+
+
+###
+
+copy env.development.txt .env.development
+
+
+
+
+
---
+++ src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+ # {{VUE_APP_SNAME}} #
+  <HelloWorld msg="Welcome to Your Vue.js App"/>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+// require('dotenv').config();
+
+// console.log('test 1234');
+const xx = process.env.NODE_ENV;
+console.log(xx);
+
+const xxx = process.env.SNAME;
+console.log(xxx);
+
+const aa = process.env;
+console.log(aa);
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+  ,
+  data() {
+   return {
+      VUE_APP_SNAME : process.env.VUE_APP_SNAME
+    }
+  },
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
